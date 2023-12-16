c3b73b2ea375f6107f6a04506db3bd8489d3936d
lrlunin <lunin@campus.tu-berlin.de>
2023-12-16 15:43:04 +0100

    rebuild structure

--- src/ZmqReceiverTest.cpp
+++ src/ZmqReceiverTest.cpp
@@ -7,6 +7,8 @@
 #include <rapidjson/stringbuffer.h>
 #include <algorithm>
 
+#include <boost/lockfree/queue.hpp>
+
 const int LENGTH = 16000;
 struct frame{
     unsigned short data[LENGTH];
@@ -23,37 +25,42 @@ struct full_data{
 };
 
 int main(){
+    boost::lockfree::queue<full_data*> boost_queue(100);
     sls::zmqHeader c;
-    std::queue<full_data*> data_queue; 
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_SUB);
     zmq::message_t payload;
     socket.connect("tcp://127.0.0.1:5555");
     socket.set(zmq::sockopt::subscribe, "");
     char counter = 0;
-    rapidjson::Document d;
-    zmq::message_t zmq_msg_1, zmq_msg_2;
+    
+    
     while (counter < 100){
+        zmq::message_t zmq_msg_1, zmq_msg_2;
         //message *msg_ptr = new message();
         // expceted to be a json
+        rapidjson::Document d;
         socket.recv(zmq_msg_1);
-
-        if (d.Parse(static_cast<char*>(zmq_msg_1.data()), zmq_msg_1.size()).HasParseError()) continue;
+        if (d.Parse(static_cast<char*>(zmq_msg_1.data()), zmq_msg_1.size()).HasParseError()){
+            std::cout << "Has parsed error" << std::endl;
+            continue;
+        };
+        std::cout << "Received json, counter: " << counter << " frameIndex " << d["frameIndex"].GetUint64() << " nextData " << (d["data"].GetUint() == 1) << std::endl;
 
         if (d["data"].GetUint() == 1){
+            std::cout << "Received data, counter: " << counter << std::endl;
             socket.recv(zmq_msg_2);
             full_data *f_data = new full_data();
             f_data->m.frameIndex = d["frameIndex"].GetUint64();
             f_data->m.bitmode = d["bitmode"].GetUint();
             std::memcpy(f_data->f.data, zmq_msg_2.data(), std::min(zmq_msg_2.size(), static_cast<size_t>(sizeof(frame::data))));
-            data_queue.push(f_data);
+            boost_queue.push(f_data);
+            ++counter;
         };
-        ++counter;
-        if (!data_queue.empty()) {
-        full_data* cur_ptr = data_queue.front();
-        std::cout << "frame_index:  " << cur_ptr->m.frameIndex << " frame avg: " << std::accumulate(cur_ptr->f.data, cur_ptr->f.data+LENGTH, 0)/LENGTH   << std::endl;
-        delete cur_ptr;
-        data_queue.pop();
+        full_data *cur_ptr;
+        if (boost_queue.pop(cur_ptr)){
+            std::cout << "frame_index:  " << cur_ptr->m.frameIndex << " frame avg: " << std::accumulate(cur_ptr->f.data, cur_ptr->f.data+LENGTH, 0)/LENGTH   << std::endl;
+            delete cur_ptr;
         }
     }
     return 0;
